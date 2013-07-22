8ed2132724174698ea7c2c25e96ce6f7be487a11
stefano_brasca <stefano_brasca@localhost>
2013-07-22 18:30:57 +0200

    New module

---
+++ src/Common_Functions.py
@@ -0,0 +1,28 @@
+###Header###############################################
+header = """
+
++------------------------------------------------------+
+ Module: Common_functions
+ Author: Stefano Brasca, Giulio Spinozzi
+ Date:  July 22th, 2013
+ Contact: brasca.stefano@hsr.it, spinozzi.giulio@hsr.it
+ Version: 0.1
++------------------------------------------------------+
+
+ Description:
+  - [...] Set of functions of common usage
+  
+ Note:
+  - [...]
+
+-------------------------------------------------------- 
+""" 
+########################################################
+
+
+###Given keys for read dictionary, it returns related lam data####################
+def get_lam (reads_data_dictionary_Key, reads_data_dictionary, lam_data_dictionay):
+    lam_data_dictionay_Key = reads_data_dictionary[reads_data_dictionary_Key][-1]
+    lam_data = lam_data_dictionay[lam_data_dictionay_Key]
+    return lam_data
+##################################################################################
